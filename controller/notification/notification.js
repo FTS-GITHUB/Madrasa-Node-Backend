38ef39f6a00bea5e9743d6bad8d595eeeeb545df
tayyabmalik4 <mtayyabmalik99@gmail.com>
2023-09-20 03:23:19 +0500

    resolve error of notification api

--- controller/notification/notification.js
+++ controller/notification/notification.js
@@ -15,9 +15,10 @@ const getAllNotifications = catchAsync(async (req, res, next) => {
 
     let CurrentUser = req.user;
     try {
-        const result = NotificationModel.find({ to: { $in: [CurrentUser?._id] } })
+        const result = await NotificationModel.find({ to: { $in: [CurrentUser?._id] } }).populate("to")
         return res.status(STATUS_CODE.OK).json({ message: SUCCESS_MSG.SUCCESS_MESSAGES.SUCCESS, result })
     } catch (err) {
+        console.log(err);
         res.status(STATUS_CODE.BAD_REQUEST).json({ message: ERRORS.PROGRAMMING.SOME_ERROR, err })
     }
 })
