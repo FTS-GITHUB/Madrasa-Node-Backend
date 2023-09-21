cb6bf9c90b2041bf9214ab5ecf2326e865301c4a
tayyabmalik4 <mtayyabmalik99@gmail.com>
2023-09-21 20:40:55 +0500

    update image extention in multer

--- utils/multer.js
+++ utils/multer.js
@@ -6,7 +6,7 @@ const upload = multer({
     storage: memoryStorage(),
     fileFilter: (req, file, callback) => {
         console.log("------------->", file)
-        if (["image/png", "image/jpeg", "application/octet-stream" , "application/pdf"].includes(file.mimetype)
+        if (["image/png", "image/jpeg","image/gif" , "image/bmp","image/tiff","image/webp", "image/svg+xml", "image/x-icon","image/heif", "image/heic", "application/octet-stream" , "application/pdf"].includes(file.mimetype)
         ) {
             callback(null, true);
         } else {
